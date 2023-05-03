5ebd82b1e58ff4ff0cc51de6792f7b019a0fec7c
Imran Asif <56230659+imranasif87@users.noreply.github.com>
2023-05-03 17:53:47 +0100

    adding dist

---
+++ dist/assets/Test_HRK_controlpanel-58c88abc.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-04fc7d56.js";import{C as e}from"./CardLayout-affb1dd1.js";const c=r=>{const{navHeader:o,children:t}=r;return a(e,{navHeader:o,children:t})};export{c as default};
