c1ee4c420590c8c9cb6ce044011aa54b65161608
Imran Asif <56230659+imranasif87@users.noreply.github.com>
2023-05-03 18:09:34 +0100

    adding dist

---
+++ assets/Evaluation-86dad88e.js
@@ -0,0 +1,1 @@
+import{c as s,C as n,j as a,L as t}from"./index-04fc7d56.js";/* empty css                     */const c=e=>{const{navHeader:r}=e;return s(n,{navHeader:r,children:[a("div",{className:"App",children:a("header",{className:"App-header",children:a("h1",{children:"Evaluering "})})}),a(t,{to:"/test",children:"Gå tilbake"})]})};export{c as default};
